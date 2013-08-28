bc9475a050e5f993153f170b6523e7107091f2ee
loyy77 <loyy77@gmail.com>
2013-08-28 08:50:17 -0700

    提交测试

---
+++ demo0/test/dao/TestAddSVN.java
@@ -0,0 +1,13 @@
+package dao;
+
+public class TestAddSVN {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("这是个测试，提交代码到svn库里。");
+	}
+
+}
